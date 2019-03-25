867ff174d5245c5d9ba3b8b8e11692b750ace9bc
iamgeniuswei <iamgeniuswei@sina.com>
2019-03-25 10:27:43 +0800

    创建视图文件，绑定URL

--- cms/cms/urls.py
+++ cms/cms/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from . import view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # url(r'^$', view.hello),
+    path('hello/', view.hello),
+    
 ]
